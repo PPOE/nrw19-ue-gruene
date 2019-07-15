6ed6b5bd7bbbcfb055d80266625997b3016bfa59
desertrold <desertrold@piratenpartei.at>
2019-07-15 18:54:36 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,8 +9,7 @@
         </div>
 
         <h2>Unterstützungserklärung für die Nationalrats&shy;wahl am 29. September</h2>
-        <p class="lead">Hier kannst du eine vorausgefüllte Unterstützungs&shy;
-        erklärung downloaden, um den Wahlantritt der Piratenpartei zu ermöglichen</p>
+        <p class="lead"> In Österreich sind alle Parteien für elektronische Unterstützungserklärungen, doch sie sind nicht rechtsgültig. Obwohl man zB. Volksbegehren digital unterschreiben kann, müssen Kleinparteien wie wir mindestens 2600 Unterstützungserklärungen binnen weniger Wochen sammeln - in jedem Bundesland, am Hauptwohnsitz, vor einem Beamten, zu regulären Öffnungszeiten. Das ist pure Schikane und verhindert den Neueinzug von Parteien, die wirklich im Bürgerauftrag und nicht durch Großspender in den Nationalrat wollen.<br>Wir zeigen Dir hier, wie einfach es wäre, Deiner Lieblingskleinpartei elektronisch den Wahlantritt zu ermöglichen.</p>
     </div>
 
 </div>
